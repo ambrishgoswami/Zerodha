749ce4afbe05b95494d3efbfddd6d63a6947c8bd
Ambrish Goswami <ambrishgoswami0@gmail.com>
2024-09-21 16:17:43 +0530

    zerodha commit

---
+++ frontend/src/landing_page/Navbar.js
@@ -0,0 +1,67 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+
+function Navbar() {
+  return (
+    <nav
+      class="navbar navbar-expand-lg border-bottom"
+      style={{ backgroundColor: "#FFF" }}
+    >
+      <div class="container p-2">
+        <Link class="navbar-brand" to="/">
+          <img
+            src="media/images/logo.svg"
+            style={{ width: "25%" }}
+            alt="Logo"
+          />
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav mb-lg-0">
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/signup">
+                  Signup
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" to="/about">
+                  About
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" to="/product">
+                  Product
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" to="/pricing">
+                  Pricing
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" to="/support">
+                  Support
+                </Link>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/landing_page/products/RightSection.js
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+
+function RightSection({ imageURL, productName, productDesription, learnMore }) {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDesription}</p>
+          <div>
+            <a href={learnMore}>Learn More</a>
+          </div>
+        </div>
+        <div className="col-6">
+          <img src={imageURL} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RightSection;
